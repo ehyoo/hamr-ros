8403fb5173147515c1aa68ed4726a635010b2932
Edward Yoo <ehyoo97@gmail.com>
2016-07-01 11:05:53 -0400

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/modlab/Documents/hamr-ros/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/modlab/Documents/hamr-ros/build"
+export PYTHONPATH="/opt/ros/indigo/lib/python2.7/dist-packages:/home/modlab/Desktop/wksp/catkin_workspace/devel/lib/python2.7/dist-packages:/home/modlab/Documents/ckbot/pyckbot/py"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/modlab/Documents/hamr-ros/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/modlab/Documents/hamr-ros/src:$ROS_PACKAGE_PATH"
